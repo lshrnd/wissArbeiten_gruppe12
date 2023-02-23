b62598abd312d0c39c725b1e31e3790a79ca38ca
lshrnd <119406635+lshrnd@users.noreply.github.com>
2023-02-23 22:57:21 +0100

    Funktionen (d),(e) und (f) hinzugefügt
    Funktionen b,c,d und f sind jediglich Tabellen bzw. Balkendiagramme, die weder viel Aussagekraft haben noch die Funktionsanforderungen genügend erfüllen. Ich habe hier leider keine weiteren Ideen, und habe Probleme bei der Umsetzung unserer Ideen. Vielleicht weiß jemand wie man das besser umsetzen kann?
    
    Funktion e faktorisiert Variablen aufgrund einer Unterteilung in Quantile. Jedoch werden nur den Quantils-Grenzen, und nicht den ganzen Intervallen das jeweilige Label zugeordnet. Auch hier weiß ich nicht wie man das lösen kann und brauche Hilfe von euch.

--- Funktionen-R-Skript 1.R
+++ Funktionen-R-Skript 1.R
@@ -18,3 +18,21 @@ funcC = function(x,y){
   table(x,y)
 }
 
+# Funktion (d) Kopie von (c)
+funcD = function(x,y){
+  table(x,y)
+}
+
+
+# Funktion (e) faktorisiert ordinale Variablen in : sehr niedrig, niedrig, mittel, hoch, sehr hoch
+# Zu Verbessern: gibt manchmal NA raus, ich weiß nicht warum
+funcE = function(x){
+  xfac = factor(x, levels = quantile(x, probs=seq(from=0,to=1,by=0.15)),
+                labels = c("sehr niedrig","niedrig","niedrig","mittel","hoch","hoch","sehr hoch"))
+  return(xfac)
+}
+
+# Funktion (f) Balkendiagramm für kategoriale Variablen
+funcF = function(x){
+  barplot(table(x))
+}
